a0a66b40b95bf8975961e4d88cab48f5de013dbf
Maria Eduarda Kolitski <mekolitski@gmail.com>
2024-11-26 21:28:22 -0300

    Commit 15

--- Movie.java
+++ Movie.java
@@ -1,37 +1,45 @@
 public class Movie {
-    private String _title;
-    private Price _price;
+    public static final int REGULAR = 0;
+    public static final int CHILDRENS = 2;
+    public static final int NEW_RELEASE = 1;
+    
+    private String title;
+    private Price price;
     
     public Movie(String title, int priceCode) {
-        _title = title;
+        this.title = title;
         setPriceCode(priceCode);
+    }
+    
+    public String getTitle() {
+        return title;
     }
 
     public int getPriceCode() {
-        return _price.getPriceCode();
+        return price.getPriceCode();
     }
 
-    public void setPriceCode(int arg) {
-        switch (arg) {
-            case Movie.REGULAR:
-                _price = new RegularPrice();
+    public void setPriceCode(int priceCode) {
+        switch (priceCode) {
+            case REGULAR:
+                price = new RegularPrice();
                 break;
-            case Movie.CHILDRENS:
-                _price = new ChildrensPrice();
+            case CHILDRENS:
+                price = new ChildrensPrice();
                 break;
-            case Movie.NEW_RELEASE:
-                _price = new NewReleasePrice();
+            case NEW_RELEASE:
+                price = new NewReleasePrice();
                 break;
             default:
-                throw new IllegalArgumentException("Incorrect Price Code");
+                throw new IllegalArgumentException("Invalid price code");
         }
     }
 
     public double getCharge(int daysRented) {
-        return _price.getCharge(daysRented);
+        return price.getCharge(daysRented);
     }
 
     public int getFrequentRenterPoints(int daysRented) {
-        return _price.getFrequentRenterPoints(daysRented);
+        return price.getFrequentRenterPoints(daysRented);
     }
 }
--- teste.java
+++ teste.java
@@ -1,5 +1,5 @@
 public class teste {
-    public class Main {
+    public static class Main {  // Torne a classe Main estática
         public static void main(String[] args) {
             Movie movie1 = new Movie("The Matrix", Movie.REGULAR);
             Movie movie2 = new Movie("The Lion King", Movie.CHILDRENS);
@@ -15,6 +15,4 @@ public class teste {
             System.out.println(customer.htmlStatement());
         }
     }
-    
-    
 }

4afc09fbfd7e0236209abbb558f17d0a94e0bb55
Maria Eduarda Kolitski <mekolitski@gmail.com>
2024-11-26 21:20:55 -0300

    Commit 12

--- ChildrensPrice.java
+++ ChildrensPrice.java
@@ -3,4 +3,18 @@ public class ChildrensPrice extends Price {
     public int getPriceCode() {
         return Movie.CHILDRENS;
     }
+
+    @Override
+    public double getCharge(int daysRented) {
+        double thisAmount = 1.5;
+        if (daysRented > 3) {
+            thisAmount += (daysRented - 3) * 1.5;
+        }
+        return thisAmount;
+    }
+
+    @Override
+    public int getFrequentRenterPoints(int daysRented) {
+        return 1;
+    }
 }
--- RegularPrice.java
+++ RegularPrice.java
@@ -3,4 +3,18 @@ public class RegularPrice extends Price {
     public int getPriceCode() {
         return Movie.REGULAR;
     }
+
+    @Override
+    public double getCharge(int daysRented) {
+        double thisAmount = 2;
+        if (daysRented > 2) {
+            thisAmount += (daysRented - 2) * 1.5;
+        }
+        return thisAmount;
+    }
+
+    @Override
+    public int getFrequentRenterPoints(int daysRented) {
+        return 1;
+    }
 }
